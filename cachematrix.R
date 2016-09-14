9e3994f12a011f0873c2e46293bf6408d44a2564
Said TALBI <said.talbi.0503@gmail.com>
2016-09-14 13:36:41 +0200

    Please my solution for programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,50 @@
 ## functions do
 
 ## Write a short comment describing this function
+# This function creates a list of four functions (see class(makeCacheMatrix(x)) 
+# and summary(makeCacheMatrix(x)) after running this code) described as follows
+# "set" function allows to cache the value of the matrix passed to it into var-
+# iable x.
+# "get" function allows to retrieve that value from cache.
+# "setinv" function allows to cache the value of the inverse to some variable 
+# myInv
+# "getinv" function allows to retriev the value of the inverse that has been 
+# cached, myInv
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    myInv <- NULL
+    set <- function(y) {
+        x <<- y
+        myInv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(vInv) myInv <<- vInv
+    getinv <- function() myInv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
+  
 }
-
 
 ## Write a short comment describing this function
+# This function takes the output of the previous function as input, checks 
+# whether its inverse has already
+# been cached. If it hasn't, it gets the value of x that has been cashed and 
+# calculatesits inverse and then returns it.
+# PS: We can see this by running twice cacheSolve() on the output of the same 
+# matrix x (the input of the first function)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  myInv <- x$getinv()
+  if(!is.null(myInv)){
+    message("Getting cached data")
+    return(myInv)
+  }
+  
+  mat2binv <- x$get()
+  myInv <- solve(mat2binv, ...)
+  x$setinv(myInv)
+  myInv
 }
+
